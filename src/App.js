a5dccc6aa0c1a444d912a24dfe45efd1f0bef246
Your Name <sdfsd@gmail.com>
2020-04-20 17:43:38 +0100

    todo list done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Todos from './todos'
+import AddTodo from './AddForm'
+class App extends Component {
+  state = {
+    todos: [
+      { id: 1, content: 'complet react course' },
+      { id: 2, content: 'finish redux' }
+    ]
+  }
+  deleteTodo =(id)=> {
+    const todos = this.state.todos.filter(todo =>{
+      return todo.id !== id 
+    })
+    this.setState({
+      todos
+    })
+  }
+  addTodo = (todo) => {
+    todo.id = this.state.todos.length+1;
+    let todos = [ ...this.state.todos,todo];
+this.setState({
+  todos: todos
+})
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  }
+  render() {
+    return (
+      <div className="todo-app container">
+        <a className='left' href='/about'>about</a>
+        <h1 className="center blue-text">My to do List</h1>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
+        <AddTodo addTodo={this.addTodo}/>
+      </div>
+    );
+  }
 }
-
 export default App;
---
+++ src/about.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const About = ()=> {
+
+    return(
+        <div className='container'>
+            <a className='left' href='/'>Home</a>
+            <h3 className='center'>About</h3>
+            <p >this just a test created by me to test react routing :D </p>
+        </div>
+    )
+
+}
+
+export default About;
